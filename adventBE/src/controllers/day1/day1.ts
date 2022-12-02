2be61d6a20e9a02f4f707b245491fb8caa3655f4
WM <will@loblollysoftware.com>
2022-12-01 20:54:10 -0500

    day 1

--- adventBE/src/controllers/day1/day1.ts
+++ adventBE/src/controllers/day1/day1.ts
@@ -1,6 +1,37 @@
 import { Request, Response } from "express";
-const day1 = (req: Request, res: Response): Response => {
-  return res.send({hey:'hi'});
+import axios from "axios";
+const day1 = async (req: Request, res: Response): Promise<Response> => {
+  const result = await axios.get("https://adventofcode.com/2022/day/1/input", {
+    headers: { cookie: process.env.SESSION_COOKIE },
+  });
+
+  const day1Input = result.data
+    .split("\n\n")
+    .map((elf: string) => elf.split("\n"));
+  let day1Output = 0;
+  const day1OutP2 = [0, 0, 0];
+
+  day1Input.forEach((elf: string[]) => {
+    const temp = elf.reduce((acc: number, cur: string) => {
+      return acc + parseInt(cur);
+    }, 0);
+    day1Output = temp > day1Output ? temp : day1Output;
+    if (temp > day1OutP2[0]) {
+      day1OutP2[0] = temp;
+    } else if (temp > day1OutP2[1]) {
+      day1OutP2[1] = temp;
+    } else if (temp > day1OutP2[2]) {
+      day1OutP2[2] = temp;
+    }
+  });
+
+  return res.send({
+    input: day1Input,
+    output1: day1Output,
+    output2: day1OutP2.reduce((acc: number, curr: number) => {
+      return acc + curr;
+    }, 0),
+  });
 };
 
 export default [{ type: "get", func: day1, path: "/day1" }];
--- adventBE/src/index.ts
+++ adventBE/src/index.ts
@@ -1,5 +1,8 @@
 import {App} from './application';
 import {routes} from './routes/router';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const port = 4000;
 
